98ae6c723348d5377a2ba27346bc1c1a87364950
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2023-07-24 10:46:12 +0200

    adds support for ios to extend beyond statusbar etc

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,12 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Hydrify',
   description: 'Hydrify is your personalized hydration partner. It helps you track, remember, conquer your water goals, and discover the path to a healthier, more refreshed you.',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    viewportFit: 'cover'
+  },
 }
 
 export default function RootLayout({ children }) {
