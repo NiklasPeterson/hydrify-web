acc0901d11cc1f566f800cac957aea64730b7594
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2023-08-23 22:07:41 +0200

    Adds support box

--- app/page.js
+++ app/page.js
@@ -6,36 +6,36 @@ export default function Home() {
       paddingLeft: "env(safe-area-inset-left)",
       paddingRight: "env(safe-area-inset-right)"
     }}>
-    <main className="flex min-h-screen flex-col items-center px-4 lg:px-0 relative" style={{
-      // backgroundImage: `url(background.png)`,
-      // backgroundRepeat: "no-repeat",
-      // backgroundSize: "cover",
-      // backgroundPosition: "top center"
-    }}>
+      <main className="flex min-h-screen flex-col items-center px-4 lg:px-0 relative" style={{
+        // backgroundImage: `url(background.png)`,
+        // backgroundRepeat: "no-repeat",
+        // backgroundSize: "cover",
+        // backgroundPosition: "top center"
+      }}>
 
-<input type="checkbox" id="overlay-input" className="lg:hidden" />
-<label for="overlay-input" id="overlay-button" className="flex lg:hidden"><span></span></label>
-  <div id="overlay">
-    <ul>
-      <li>
-        <a
-            className="flex place-items-center py-2"
-            href="https://x.com/AppHydrify"
-            rel="noopener noreferrer"
-            target='_blank'
-          >
-            <Image
-              src="/x.svg"
-              alt="Follow Hydrify on X"
-              width={24}
-              height={24}
-              priority
-              className="mr-2"
-            />
-            Follow
-          </a>
-        </li>
-        {/* <li>
+        <input type="checkbox" id="overlay-input" className="lg:hidden" />
+        <label for="overlay-input" id="overlay-button" className="flex lg:hidden"><span></span></label>
+        <div id="overlay">
+          <ul>
+            <li>
+              <a
+                className="flex place-items-center py-2"
+                href="https://x.com/AppHydrify"
+                rel="noopener noreferrer"
+                target='_blank'
+              >
+                <Image
+                  src="/x.svg"
+                  alt="Follow Hydrify on X"
+                  width={24}
+                  height={24}
+                  priority
+                  className="mr-2"
+                />
+                Follow
+              </a>
+            </li>
+            {/* <li>
         <a
             className="flex place-items-center py-2"
             href="https://www.producthunt.com/products/hydrify"
@@ -53,29 +53,29 @@ export default function Home() {
             Follow on Product Hunt
           </a>
         </li> */}
-      <li>
-        <a
-            className="flex place-items-center py-2"
-            href="https://www.buymeacoffee.com/niklaspeterson/e/146387"
-            rel="noopener noreferrer"
-            target='_blank'
-          >
-            <Image
-              src="/bmc.svg"
-              alt="Support Hydrify on Buy Me Coffee"
-              width={24}
-              height={24}
-              priority
-              className={`mr-2`}
-            />
-            Support Hydrify
-          </a>
-        </li>
-    </ul>
-  </div>
+            <li>
+              <a
+                className="flex place-items-center py-2"
+                href="https://www.buymeacoffee.com/niklaspeterson/e/146387"
+                rel="noopener noreferrer"
+                target='_blank'
+              >
+                <Image
+                  src="/bmc.svg"
+                  alt="Support Hydrify on Buy Me Coffee"
+                  width={24}
+                  height={24}
+                  priority
+                  className={`mr-2`}
+                />
+                Support Hydrify
+              </a>
+            </li>
+          </ul>
+        </div>
 
-  
-      <nav className="z-10 w-full max-w-[1440px] lg:px-28 items-center justify-between flex h-20 gap-4">
+
+        <nav className="z-10 w-full max-w-[1440px] lg:px-28 items-center justify-between flex h-20 gap-4">
           <a
             className="flex place-items-center"
             href="https://hydrify.app"
@@ -91,82 +91,112 @@ export default function Home() {
             />
           </a>
 
-        <div className='hidden lg:flex lg:flex-row lg:gap-8'>
+          <div className='hidden lg:flex lg:flex-row lg:gap-8'>
             <a
-            className="flex place-items-center py-2 hover:opacity-80"
+              className="flex place-items-center py-2 hover:opacity-80"
+              href="https://x.com/AppHydrify"
+              rel="noopener noreferrer"
+              target='_blank'
+            >
+              <Image
+                src="/x.svg"
+                alt="Follow Hydrify on x"
+                width={24}
+                height={24}
+                priority
+                className={`mr-2`}
+              />
+              Follow
+            </a>
+
+            <a
+              className="flex place-items-center py-2 hover:opacity-80"
+              href="https://www.producthunt.com/products/hydrify"
+              rel="noopener noreferrer"
+              target='_blank'
+            >
+              <Image
+                src="/ph.svg"
+                alt="Upvote Hydrify on Product Hunt"
+                width={24}
+                height={24}
+                priority
+                className={`mr-2`}
+              />
+              Upvote
+            </a>
+
+            <a
+              className="flex place-items-center py-2 hover:opacity-80"
+              href="https://www.buymeacoffee.com/niklaspeterson/e/146387"
+              rel="noopener noreferrer"
+              target='_blank'
+            >
+              <Image
+                src="/bmc.svg"
+                alt="Sponsor Hydrify on BuyMeCoffee"
+                width={24}
+                height={24}
+                priority
+                className={`mr-2`}
+              />
+              Sponsor Hydrify
+            </a>
+          </div>
+        </nav>
+
+        <div className="flex flex-1 flex-col items-center justify-center text-black">
+          <h1 className={`mb-3 text-center font-medium text-4xl lg:text-6xl lg:leading-tight`}>
+            Stay hydrated, stay healthy <br />
+            with <span className={`text-white`}>Hydrify.</span>
+          </h1>
+          <p className={`mt-4 text-center font-normal text-xl max-w-[480px] lg:max-w-[800px] lg:text-2xl`}>
+            Hydrify is the ultimate hydration companion app designed to guide you on your journey to optimal health and wellness.
+          </p>
+
+          <div className='mt-8 flex flex-row gap-4'>
+
+            <a
+              href='https://www.producthunt.com/products/hydrify'
+              target='_blank'
+              className={`bg-white border border-white text-[#52CCC1] font-semibold text-base h-12 px-4 flex items-center justify-center rounded-lg hover:opacity-80`}>
+              Get notified!
+            </a>
+
+            {/* <a href="https://apple.com" rel='noreferrer' target='_blank' className='hover:opacity-80'>
+              <Image
+                src='/App_store-black.svg'
+                alt='Follow Hydrify on X'
+                width={145}
+                height={48}
+                priority
+                className='pointer-events-none select-none cursor-pointer'
+              />
+            </a> */}
+
+          </div>
+        </div>
+
+        <div className='flex flex-col pb-6 pt-12 px-6 bg-white rounded-3xl text-center lg:w-80 lg:absolute lg:bottom-4 lg:right-4'>
+          <h4 className='text-2xl text-black mb-2 font-semibold'>Support & Feedback</h4>
+
+          <p className='text-base text-[#85858B]'>If you have any questions or suggestions reach out to Hydrify on Twitter/X.</p>
+
+          <a
+            className='bg-[#52CCC1] text-white text-base rounded-full flex items-center justify-center h-12 mt-6 hover:opacity-80'
             href="https://x.com/AppHydrify"
             rel="noopener noreferrer"
             target='_blank'
           >
-            <Image
-              src="/x.svg"
-              alt="Follow Hydrify on x"
-              width={24}
-              height={24}
-              priority
-              className={`mr-2`}
-            />
-            Follow
-            </a>
-
-            {/* <a
-            className="flex place-items-center py-2 hover:opacity-80"
-            href="https://www.producthunt.com/products/hydrify"
-            rel="noopener noreferrer"
-            target='_blank'
-          >
-            <Image
-              src="/ph.svg"
-              alt="Follow Hydrify on Product Hunt"
-              width={24}
-              height={24}
-              priority
-              className={`mr-2`}
-            />
-            Follow
-            </a> */}
-            
-            <a
-            className="flex place-items-center py-2 hover:opacity-80"
-            href="https://www.buymeacoffee.com/niklaspeterson/e/146387"
-            rel="noopener noreferrer"
-            target='_blank'
-          >
-            <Image
-              src="/bmc.svg"
-              alt="Support Hydrify on BuyMeCoffee"
-              width={24}
-              height={24}
-              priority
-              className={`mr-2`}
-            />
-            Support Hydrify
-            </a>
-        </div>
-      </nav>
-
-      <div className="flex flex-1 flex-col items-center justify-center text-black">
-          <h1 className={`mb-3 text-center font-medium text-4xl lg:text-6xl lg:leading-tight`}>
-          Stay hydrated, stay healthy <br/>
-          with <span className={`text-white`}>Hydrify.</span>
-          </h1>
-          <p className={`mt-4 text-center font-normal text-xl max-w-[480px] lg:max-w-[800px] lg:text-2xl`}>
-          Hydrify is the ultimate hydration companion app designed to guide you on your journey to optimal health and wellness.
-          </p>
-          <a
-          a 
-          href='https://www.producthunt.com/products/hydrify'
-          target='_blank'
-          className={`bg-white border border-white text-[#52CCC1] hover:opacity-80 font-semibold py-4 px-8 rounded-full mt-8`}>
-            {/* className={`bg-white border border-white text-[#52CCC1] hover:bg-[#52CCC1] hover:text-white hover:opacity-80 font-semibold py-4 px-8 rounded-full mt-8`}> */}
-            Sign up
+            Contact
           </a>
-      </div>
-    <footer className='w-full max-w-[1440px] lg:px-28 items-center justify-center flex h-20'>
-      <span>© 2023 Hydrify | Created by <a href='https://x.com/Niklas_Peterson' className='hover:opacity-80'>Niklas Peterson</a>.</span>
-      {/* <a href="https://www.producthunt.com/posts/hydrify?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-hydrify" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=406695&theme=light" alt="Hydrify - Track&#0044;&#0032;get&#0032;reminded&#0044;&#0032;and&#0032;conquer&#0032;your&#0032;daily&#0032;water&#0032;intakes&#0033; | Product Hunt" style={{width: 250, height: 54}} width="250" height="54" /></a> */}
-    </footer>
-    </main>
+        </div>
+
+        <footer className='w-full max-w-[1440px] lg:px-28 items-center justify-center flex h-20'>
+          <span>© 2023 Hydrify | Created by <a target='_blank' href='https://x.com/Niklas_Peterson' className='underline hover:opacity-80'>Niklas Peterson</a>.</span>
+          {/* <a href="https://www.producthunt.com/posts/hydrify?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-hydrify" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=406695&theme=light" alt="Hydrify - Track&#0044;&#0032;get&#0032;reminded&#0044;&#0032;and&#0032;conquer&#0032;your&#0032;daily&#0032;water&#0032;intakes&#0033; | Product Hunt" style={{width: 250, height: 54}} width="250" height="54" /></a> */}
+        </footer>
+      </main>
     </div>
   )
 }
