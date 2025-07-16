0371c8d30e1e32c973a82b78f3a3003ab532d6e4
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2025-07-16 21:33:25 +0200

    Refactor viewport settings into a separate export in layout.js for improved organization

--- app/layout.js
+++ app/layout.js
@@ -7,20 +7,18 @@ export const metadata = {
   title: 'Hydrify',
   description: 'Hydrify is your go-to assistant for staying refreshed. With personalized goals and tailored reminders, it ensures you drink enough water daily. Achieve well-being and feel rejuvenated effortlessly with Hydrify.  ',
   metadataBase: new URL('https://hydrify.app'),
-
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    viewportFit: 'cover'
-  },
-
   other: {
     'apple-itunes-app': 'app-id=6450311759, app-argument=https://apps.apple.com/app/hydrify/id6450311759'
   },
   "manifest": "/manifest.webmanifest"
-  
 }
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: 'cover'
+};
 
 
 export default function RootLayout({ children }) {
